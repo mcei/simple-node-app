6ae33d4323824a5d74fa74053140dd66edac7f25
mcei <ks@acer.com>
2021-11-28 21:21:29 +0700

    Response message modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hi therre');
+	res.send('This is simple node app');
 });
 
 app.listen(8080, () => {
